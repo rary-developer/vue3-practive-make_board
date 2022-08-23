3d36f5a2cf43f6e2ef045275065d2b4d652842a2
rary <xogml6915@gmail.com>
2022-08-23 23:35:07 +0900

    vue3 연습 게시판 만들기 day2

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ import PostCreateView from '@/views/posts/PostCreateView.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import PostEditView from '@/views/posts/PostEditView.vue';
 import PostListView from '@/views/posts/PostListView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import NestedView from '@/views/nested/NestedView.vue';
+import NestedOneView from '@/views/nested/NestedOneView.vue';
+import NestedTwoView from '@/views/nested/NestedTwoView.vue';
+import NestedHomeView from '@/views/nested/NestedHomeView.vue';
 
 const routes = [
 	{
@@ -19,10 +24,12 @@ const routes = [
 	},
 	{
 		path: '/posts',
+		name: 'PostList',
 		component: PostListView,
 	},
 	{
 		path: '/posts/create',
+		name: 'PostCreate',
 		component: PostCreateView,
 	},
 	{
@@ -30,11 +37,40 @@ const routes = [
 		// /user/bruce -> PostDetailView
 		// /user/tom
 		path: '/posts/:id',
+		name: 'PostDetail',
 		component: PostDetailView,
 	},
 	{
 		path: '/posts/:id/edit',
+		name: 'PostEdit',
 		component: PostEditView,
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: NotFoundView,
+	},
+	{
+		path: '/nested',
+		name: 'Nested',
+		component: NestedView,
+		children: [
+			{
+				path: '',
+				name: 'NestedHome',
+				component: NestedHomeView,
+			},
+			{
+				path: 'one',
+				name: 'NestedOne',
+				component: NestedOneView,
+			},
+			{
+				path: 'two',
+				name: 'NestedTwo',
+				component: NestedTwoView,
+			},
+		],
 	},
 ];
 
